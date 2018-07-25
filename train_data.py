5dd371409046f5ce0e34e47ecbc3b0813bf7c9d5
Karthikeyan A <karthikeyana97@gmail.com>
2018-07-25 23:35:29 +0530

    extracting data final

--- train_data.py
+++ train_data.py
@@ -1,40 +1,59 @@
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.datasets import load_files
-
-
-# Reading the train and test meta-data files
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
-
-#train.head()
-
-
-TRAIN_PATH = 'train/'
-TEST_PATH = 'test/'
-
-
-img_path = TRAIN_PATH+str(train.Image_id[0])
-
-
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
 from PIL import Image
 import cv2
 
 
-from tqdm import tqdm
+# Reading the train data and meta-data files
+
+TRAIN_PATH = 'meta/'
+TRAIN_IMG_PATH = "images/"
+f = open(TRAIN_PATH+"train.txt","r")
+
+lists = f.readlines()  
+
+
+
+
 def read_img(img_path):
+    
     img = cv2.imread(img_path)
-    img = cv2.resize(img, (384, 384))
+    try:
+        img = cv2.resize(img, (128,128),interpolation = cv2.INTER_AREA)
+    except:
+        return None
     return img
+    
+
+def train_data_gen():
+    train_img = []
+    lbl = []
+    for img_path in lists: #lists:
+        im = read_img(TRAIN_IMG_PATH + img_path.replace("\n","")+".jpg")
+        if im is not None:
+            name = img_path.split('/')[0]
+            lbl.append(name)
+            train_img.append(im)
 
 
 
-train_img = []
-for img_path in train.Image_id.values:
-    train_img.append(read_img(TRAIN_PATH + img_path))
+    np.save('train_data_128.npy', train_img)
+    np.save('train_data_lbl.npy', lbl)
 
+def one_hot_array_gen():
+    lbl = np.load('train_data_lbl.npy')
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(lbl)
+    print(integer_encoded)
 
+    data = integer_encoded
+    #data = np.array(data)
+    # one hot encode
+    encoded = to_categorical(data)
+    print(encoded[0])
+    np.save('train_data_lbl_oha.npy', encoded)
 
-np.save('train_data.npy', train_img)
+one_hot_array_gen()
+

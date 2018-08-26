2e84de50ced5d191bacdbd266c89575168304298
Karthikeyan A <karthikeyana97@gmail.com>
2018-08-26 07:44:25 +0530

    final commit

--- model1_1.py
+++ model1_1.py
@@ -23,104 +23,68 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import regularizers
 
 def bulidModel():
-    weight_decay = 1e-4
+    
+    weight_decay = 1e-3
     model = Sequential()
     model.add(BatchNormalization(input_shape=(128,128,3)))
-    model.add(Conv2D(8, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    
-    model.add(Conv2D(16, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    
-    model.add(Conv2D(32, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    
-    model.add(Conv2D(64, kernel_size=(3, 3), activation= 'relu', padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
     model.add(Conv2D(64, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-    
+    model.add(Conv2D(64, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
+ 
+    model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+
+  
     model.add(Conv2D(128, kernel_size=(3, 3), activation= 'relu', padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
     model.add(Conv2D(128, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.4))
+    model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
 
     model.add(Conv2D(256, kernel_size=(3, 3), activation= 'relu', padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
-    #model.add(Conv2D(256, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.5))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation= 'relu',kernel_regularizer=regularizers.l2(weight_decay)))
+   
+    model.add(GlobalAveragePooling2D())
     
-    model.add(Flatten())
-        
-    model.add(Dense(101))
+    model.add(Dense(1024, activation='relu'))
+    model.add(BatchNormalization())
+     
+    model.add(Dense(10))
     model.add(Activation('softmax'))
-    #from keras import backend as K
-    #model.add(Lambda(lambda x: K.tf.nn.softmax(x)))
+
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
+    print(model.summary())
     
     return model
     
 def load_model(model):
-    model.load_weights('weights.best.eda.hdf5')
+    model.load_weights('new_try5_9.h5')
 
 def train(model):
     batch_size = 16
 
-    # this is the augmentation configuration we will use for training
-    train_datagen = ImageDataGenerator(
-            #rescale=1./255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
 
-    # this is the augmentation configuration we will use for testing:
-    # only rescaling
-    #test_datagen = ImageDataGenerator(rescale=1./255)
-
-    x = np.load('train_data_128.npy')
+    x = np.load('train_data_128_10_3.npy')
               
-    # Finally, we create the training and validation sets.
-    #x=x/255
-
-    #mean = x.mean(axis=(0,1,2,3))
-    #std = x.std(axis=(0,1,2,3))
-    #x=(x-mean)/(std+le-7)          
-              
-    y = np.load('train_data_lbl_oha.npy')
-    #y = np.array(y)
-    Xtrain, Xvalid, ytrain, yvalid = train_test_split(x, y, test_size=0.33)
-    model.fit_generator(
-            train_datagen.flow(Xtrain,ytrain,batch_size=16),
-            steps_per_epoch=(66667 // batch_size),
-            epochs=5,
-            validation_data=(Xvalid,yvalid),
-            validation_steps=(33333 // batch_size))
-    model.save_weights('first_try.h5')  # always save your weights after training or during training//
-
   
-    #early_stops = EarlyStopping(patience=3, monitor='val_acc')
-    #checkpointer = ModelCheckpoint(filepath='weights.best.eda.hdf5', verbose=1, save_best_only=True)
+    y = np.load('train_data_lbl_oha3.npy')
+    Xtrain, Xvalid, ytrain, yvalid = train_test_split(x, y, test_size=0.30)
 
 
-    # Finally, we train our model.
+    #  train our model.
 
     
-    #model.fit(Xtrain, ytrain, validation_data=(Xvalid, yvalid), epochs=3, batch_size=1, callbacks=[checkpointer], verbose=1)
 
+    model.fit(Xtrain, ytrain, validation_data=(Xvalid, yvalid), epochs=50, batch_size=16,  verbose=1)
+    model.save_weights('new_try5_10.h5')  # always save your weights after training 
 def test_data(model):
-    test_data = np.load('test_data.npy')
+    test_data = np.load('test_data_128_10_3.npy')
     fig=plt.figure()
-    d = np.load("lables.npy")
+    fig=plt.plot()
+    d = ['Cake','Donuts','fries','rice','hamburger','ice_cream','omelette','pizza','samosa','rolls']
     # one hot encode
-    model_out = model.predict(test_data[:12])
-        
-
+    model_out = model.predict_classes(test_data)
+    
     for i in range(12):
         
-        #img_num = data[1]
-        img_data = test_data[i]
+        
+        img_data = test_data[i+1000]
         
         y = fig.add_subplot(3,4,i+1)
         orig = img_data
@@ -133,24 +97,18 @@ def test_data(model):
 
 
 
-f=h5py.File('food_c101_n1000_r384x384x3.h5','r')  
-
-
-y=np.array([[int(i) for i in f["category"][j]] for j in range(len(f["category"]))])
-    
-
-print(y[0])    
-    
 ###buliding model
 model = bulidModel()
 ##
+load_model(model)
+
 ###training model
 model = train(model)
 ##
 ###loading from saved model
-##load_model(model)
 ##
 ###testing data
-##test_data(model)
+test_data(model)
 ##
 ##
+
